b95d8327bc3bb8ac1237a2f71c3fca2c981c80da
Eric <escohen98@gmail.com>
2019-01-09 13:52:47 -0800

    Created helloios app. Added screenshots folder.

---
+++ helloios/helloios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  helloios
+//
+//  Created by Eric S. Cohen on 1/9/19.
+//  Copyright © 2019 Eric S. Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
